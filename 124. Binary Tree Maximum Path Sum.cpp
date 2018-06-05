2ff183bc2ec9cb153990a162617b9508eeab9112
ccw2010 <39959438+ccw2010@users.noreply.github.com>
2018-06-05 12:57:42 -0700

    Update 124. Binary Tree Maximum Path Sum.cpp

--- 124. Binary Tree Maximum Path Sum.cpp
+++ 124. Binary Tree Maximum Path Sum.cpp
@@ -2,15 +2,21 @@ class Solution {
 public:
     int maxPathSum(TreeNode *root) {
         int res = root->val;
-        maxPathSumDFS(root, res);
+        dfs(root, res);
         return res;
     }
-    int maxPathSumDFS(TreeNode *root, int &res) {
+    int dfs(TreeNode *root, int &res) {
         if (!root) return 0;
-        int left = maxPathSumDFS(root->left, res);
-        int right = maxPathSumDFS(root->right, res);
-        int top = root->val + (left > 0 ? left : 0) + (right > 0 ? right : 0);
-        res = max(res, top);
-        return max(left, right) > 0 ? max(left, right) + root->val : root->val; 
+        int left = dfs(root->left, res);
+        int right = dfs(root->right, res);
+        int sum = root->val + max(left, 0) + max(right, 0);
+        res = max(res, sum);
+        return max(max(left, right) + root->val, root->val); 
     }
 };
+
+
+
+
+
+
