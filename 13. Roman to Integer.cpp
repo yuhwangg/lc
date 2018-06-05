9d128ea6f89ab4144f2b54ea8881094ea173758e
wang <yxw9636@gmail.com>
2018-06-05 16:21:57 -0700

    Update 13. Roman to Integer.cpp

--- 13. Roman to Integer.cpp
+++ 13. Roman to Integer.cpp
@@ -100,11 +100,17 @@ public:
     int romanToInt(string s) {
         int res = 0;
         unordered_map<char, int> m{{'I',1}, {'V',5}, {'X',10}, {'L',50}, {'C',100}, {'D',500}, {'M',1000}};
-        for (int i=0; i<s.size(); i++){
+        int n = s.size();
+        for (int i=0; i<n; i++){
             int val = m[s[i]];
-            if(i==s.size()-1 || m[s[i+1]]<=val) res += val;
+            if(i==n-1 || m[s[i+1]]<=val) res += val;
             else res -= val;
         }
         return res;
     }
 };
+
+
+
+
+
